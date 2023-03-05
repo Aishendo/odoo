7f14c3f593a34fef920ad42999306df5d63aaddf
Aishendo <89102440+Aishendo@users.noreply.github.com>
2023-03-06 01:56:20 +0600

    First commit

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -100,
+    'summary': 'Hospital Management System',
+    'description': """Hospital Management System""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
